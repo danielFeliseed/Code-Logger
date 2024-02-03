a44d9c5076b73f71c5441ad24f6c97354a21e59f
danielFeliseed <daniel@feliseed.co.jp>
2024-02-03 16:44:44 +0900

    centered blog posts

--- resources/js/Components/Post.vue
+++ resources/js/Components/Post.vue
@@ -7,16 +7,22 @@ defineProps({
 </script>
 
 <template>
-<div class="flex items-start gap-2.5">
-    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese image">
-    <div class="flex flex-col w-full max-w-[520px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+<div class=" flex flex-col items-center mb-3">
+    <div class="flex items-start gap-2.5">
+        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese image">
+        <div class="flex flex-col w-full max-w-[520px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-xl dark:bg-gray-700">
        <div class="flex items-center space-x-2 rtl:space-x-reverse">
           <span class="text-sm font-semibold text-gray-900 dark:text-white">{{ post.user.name }}</span>
           <span class="text-sm font-normal text-gray-500 dark:text-gray-400">{{new Date(post.created_at).toLocaleDateString()}}</span>
        </div>
+       <div>
        <h3 class="text-xl text-white whitespace-nowrap">{{ post.title }}</h3>
        <p  class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{{ post.body }}</p>
        
+        </div>
+        <div>
+            <p>{{ comment.body }}</p>
+        </div>
     </div>
     <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" data-dropdown-placement="bottom-start" class="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600" type="button">
        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
@@ -43,4 +49,5 @@ defineProps({
        </ul>
     </div>
  </div>
+</div>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -19,16 +19,13 @@ const props = defineProps({
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
     <div class="flex flex-col">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
+        <div class="flex items-center justify-between w-full mb-3">
+            <h2 class="text-2xl font-semibold text-gray-800">Blog posts</h2>
         </div>
-        <div class=" justify-center" v-for="post in posts" :key="post.id ">
-              <Post :post="post"/>
-        
+        <div class=" justify-center" v-for="post in posts" :key="post.id" >
+            <div v-for="comment in post.comments" :key="comment.id">
+                <Post :post="post" :comment="comment"/>
+            </div>
         </div>
     </div>
 
