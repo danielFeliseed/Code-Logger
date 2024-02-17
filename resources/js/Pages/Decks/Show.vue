c81930510d7d721e2f44f96ff30960788cd00f9a
danielFeliseed <daniel@feliseed.co.jp>
2024-02-17 15:37:29 +0900

    feat: added card details modal

--- resources/js/Pages/Decks/Show.vue
+++ resources/js/Pages/Decks/Show.vue
@@ -15,11 +15,29 @@ let showBackOfCard = ref(false);
 
 let toggleCards = ref(false);
 let toggleReviewButton = ref(true);
+let showCardDetails = ref(false);
+let cardDetailsToBeShown = ref(
+    {
+        front: '',
+        back: '',
+        id: '',
+    }
+);
 
 function toggleCardsAndReviewButton() {
     toggleCards.value = !toggleCards.value;
     toggleReviewButton.value = !toggleReviewButton.value;
     cardDisplays.value = !cardDisplays.value;
+}
+
+function toggleCardDetails(card = null) {
+    cardDetailsToBeShown.value = {
+        front: card.front,
+        back: card.back,
+        id: card.id,
+    }
+    showCardDetails.value = !showCardDetails.value;
+
 }
 
 const flipCard = () => {
@@ -34,59 +52,73 @@ console.log(page.props);
     <AuthenticatedLayout>
     
     <div class="mx-10 ">
-        
         <div class=" flex justify-between mt-5 ">
-            <Link href="./" class="text-gray-200 text-2xl">Cards in {{ deckName }}</Link>
+            <Link href="./" class="text-gray-200 text-2xl hover:text-gray-400">Cards in {{ deckName }}</Link>
             <button v-if="cards.length !== 0" v-show="toggleReviewButton" @click="toggleCardsAndReviewButton()" class=" bg-slate-950 hover:bg-black rounded py-1 px-5 text-gray-200">Review cards</button>
         </div>
         <div v-if="cards.length == 0" class="flex flex-col justify-center items-center ">
-            <h1 class="text-3xl text-gray-200 self-center justify-center text-center mt-28">No cards in this deck. <br> You can create them with the Card Creator</h1>
+            <h1 class="text-3xl text-gray-200 self-center justify-center text-center mt-28">No cards in this deck. <br> You can create them with the <Link href="/dashboard" class=" underline text-white hover:text-gray-400 ">Card Creator</Link></h1>
         </div>
-        <div v-else class="grid grid-cols-3 justify-center items-center">
-            <ul v-show="cardDisplays" v-for="card in cards" >
+        <div v-else class=" flex flex-col lg:grid lg:grid-cols-3 justify-center items-center">
+            <ul v-show="cardDisplays" v-for="card in cards" :key="card.id">
                 <li class=" bg-slate-950 hover:bg-black text-white text-center border rounded-lg rounded-b-none p-2 mt-7 pt-10 w-[300px] h-28" >{{ card.front }}</li>
-                <button class="text-white border bg-slate-800 hover:bg-black px-1 rounded-b-sm w-[150px]">Details</button>
+                <button @click="toggleCardDetails(card)" class="text-white border bg-slate-800 hover:bg-black px-1 rounded-b-sm w-[150px]">Details</button>
                 <button class="text-white border bg-slate-800 hover:bg-red-800 px-1 rounded-b-sm w-[150px]">Delete</button>
             </ul>
         </div>
-    </div>
+    
 
-    <div class="mx-7 flex flex-col items-center justify-center">
-       
-       
-        <div v-show="toggleCards">
-            <transition name="slide-fade">
-            <div id="front-of-card" v-show="showFrontOfCard" class="flex justify-center items-center mt-16">
-                <div class="  border-2 border-b-2 border-gray-700 bg-gray-900 hover:shadow-xl hover:w-[405px] hover:h-[505px] rounded-md  w-[400px] h-[500px]">
-                    <div class="h-0 flex justify-between">
-                        <p class=" text-gray-200 font-semibold mx-5 py-3">Card 1 of 20</p>
-                        <p class=" text-gray-200 font-semibold mx-5 py-3">Time elapsed: 06</p>
-                    </div>
-                    <div class=" bg-zinc-950 border-t-2 border-b-2 mt-[60px] h-3/4 text-center flex items-center justify-center">
-                        <p class=" text-gray-200">What does MVC stand for in Laravel?</p>
-                    </div>
-                    <button @click="flipCard" class=" w-1/3 text-gray-200 bg-slate-800 hover:bg-green-700 h-16 border-r-2">Easy</button>
-                    <button @click="flipCard" class=" w-1/3 text-gray-200 bg-slate-800 hover:bg-yellow-700 h-16 border-r-2">Medium</button>
-                    <button @click="flipCard" class=" w-1/3 text-gray-200 bg-slate-800 hover:bg-red-700 h-16">Hard</button>
+        <div v-show="showCardDetails" class=" flex justify-center mt-16">
+            <div class=" flex flex-col gap-4 border rounded-lg items-center pt-10 pb-5 px-10 text-center bg-slate-950 w-[450px]" >
+                <h1 class="text-3xl text-gray-200">Card details</h1>
+                <label class="text-gray-200 font-semibold text-lg" for="card-front">Front of card</label>
+                <div class=" text-gray-200 w-[400px] border rounded-lg py-3 px-3 bg-black">
+                    {{ cardDetailsToBeShown.front }}
+                </div>
+                <label class="text-gray-200 font-semibold text-lg" for="card-back">Back of card</label>
+                <div class=" text-gray-200  w-[400px] border rounded-lg py-3 px-3 bg-black">
+                    {{ cardDetailsToBeShown.back }}
+                </div>
+                <div>
+                    <button @click="toggleCardDetails" class="text-white border bg-slate-800 hover:bg-black px-1 mt-3 rounded-b-sm w-[150px]">Close</button>
                 </div>
             </div>
-            </transition>
-            
-            <transition name="slide-fade">
-            <div v-show="showBackOfCard" id="back-of-card" class=" justify-center items-center">
-                <div class="  border-2 border-b-2 border-white bg-slate-950 hover:shadow-2xl hover:w-[405px] hover:h-[505px] rounded-md  w-[400px] h-[500px]">
-                    <div class="h-0 flex justify-center items-center">
-                        <p class=" text-gray-200 font-semibold mx-5 mt-14 text-sm">It took you 8 seconds to answer</p>
-                    </div>
-                    <div class=" bg-zinc-950 border-t-2 border-b-2 mt-[60px] h-3/4 text-center flex items-center justify-center">
-                        <p class=" text-gray-200">Model View Controller</p>
-                    </div>
-                    <button @click="flipCard" id="next-card-button" class=" w-full text-gray-200 bg-slate-800 h-16 border-r-2">Next card</button>
-                </div>
-            </div>
-            </transition>
         </div>
-            
+
+        <div class="mx-7 flex flex-col items-center justify-center">
+            <div v-show="toggleCards">
+                <transition name="slide-fade">
+                <div id="front-of-card" v-show="showFrontOfCard" class="flex justify-center items-center mt-16">
+                    <div class="  border-2 border-b-2 border-gray-700 bg-gray-900 hover:shadow-xl hover:w-[405px] hover:h-[505px] rounded-md  w-[400px] h-[500px]">
+                        <div class="h-0 flex justify-between">
+                            <p class=" text-gray-200 font-semibold mx-5 py-3">Card 1 of 20</p>
+                            <p class=" text-gray-200 font-semibold mx-5 py-3">Time elapsed: 06</p>
+                        </div>
+                        <div class=" bg-zinc-950 border-t-2 border-b-2 mt-[60px] h-3/4 text-center flex items-center justify-center">
+                            <p class=" text-gray-200">What does MVC stand for in Laravel?</p>
+                        </div>
+                        <button @click="flipCard" class=" w-1/3 text-gray-200 bg-slate-800 hover:bg-green-700 h-16 border-r-2">Easy</button>
+                        <button @click="flipCard" class=" w-1/3 text-gray-200 bg-slate-800 hover:bg-yellow-700 h-16 border-r-2">Medium</button>
+                        <button @click="flipCard" class=" w-1/3 text-gray-200 bg-slate-800 hover:bg-red-700 h-16">Hard</button>
+                    </div>
+                </div>
+                </transition>
+                
+                <transition name="slide-fade">
+                <div v-show="showBackOfCard" id="back-of-card" class=" justify-center items-center">
+                    <div class="  border-2 border-b-2 border-white bg-slate-950 hover:shadow-2xl hover:w-[405px] hover:h-[505px] rounded-md  w-[400px] h-[500px]">
+                        <div class="h-0 flex justify-center items-center">
+                            <p class=" text-gray-200 font-semibold mx-5 mt-14 text-sm">It took you 8 seconds to answer</p>
+                        </div>
+                        <div class=" bg-zinc-950 border-t-2 border-b-2 mt-[60px] h-3/4 text-center flex items-center justify-center">
+                            <p class=" text-gray-200">Model View Controller</p>
+                        </div>
+                        <button @click="flipCard" id="next-card-button" class=" w-full text-gray-200 bg-slate-800 h-16 border-r-2">Next card</button>
+                    </div>
+                </div>
+                </transition>
+            </div>
+        </div>
     </div>
 
     </AuthenticatedLayout>
