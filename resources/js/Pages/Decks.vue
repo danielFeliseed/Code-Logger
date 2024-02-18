05a56962be74ecc55bb089488000cabd8946b3d6
danielFeliseed <daniel@feliseed.co.jp>
2024-02-18 09:14:23 +0900

    fix: centered delete modal

--- resources/js/Pages/Decks.vue
+++ resources/js/Pages/Decks.vue
@@ -62,9 +62,9 @@ console.log(page.props);
             <button @click="toggleDeleteModal(deck.id)" data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="text-gray-200 hover:bg-red-800 text-center border mx-auto bg-slate-800 px-2 py-1 w-1/3  mt-3 rounded-md">Delete</button>
         </div>
         <!-- Delete Modal -->
-        <div v-show="showDeleteModal" id="popup-modal" tabindex="-3" class=" overflow-y-auto overflow-x-hidden relative z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div class=" p-4 w-full max-w-md max-h-full">
-                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div v-show="showDeleteModal" id="popup-modal" tabindex="-3" class="flex overflow-y-auto overflow-x-hidden absolute z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class=" p-4 w-full max-w-md max-h-full ">
+                    <div class="relative rounded-lg shadow dark:bg-gray-700">
                         <button @click="toggleDeleteModal()"  type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
